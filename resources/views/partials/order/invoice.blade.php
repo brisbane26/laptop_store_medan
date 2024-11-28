c84ac710d28f755c0528b00152a9c6960627c3fd
petrr4 <petraik111@gmail.com>
2024-11-28 11:25:11 +0700

    new invoice

--- resources/views/partials/order/invoice.blade.php
+++ resources/views/partials/order/invoice.blade.php
@@ -1,59 +1,76 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Invoice #{{ $order->id }}</title>
     <style>
+        html,
         body {
-
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f4f4f9;
+            margin: 10px;
+            padding: 10px;
+            font-family: sans-serif;
+        }
+        h1,h2,h3,h4,h5,h6,p,span,label {
+            font-family: sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 0px !important;
+        }
+        table thead th {
+            height: 28px;
+            text-align: left;
+            font-size: 16px;
+            font-family: sans-serif;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            font-size: 14px;
         }
 
+        .heading {
+            font-size: 24px;
+            margin-top: 12px;
+            margin-bottom: 12px;
+            font-family: sans-serif;
+        }
+        .small-heading {
+            font-size: 18px;
+            font-family: sans-serif;
+        }
+        .total-heading {
+    font-size: 20px;
+    background-color: #f2f2f2;
+    padding: 10px;
+    border-top: 2px solid #414ab1;
+}
 
-            .invoice-container {
-        max-width: 800px;
-        margin: 30px auto;
-        background-color: white;
-        border: 1px solid #ddd;
-        border-radius: 8px;
-        padding: 20px;
-    }
+        .order-details tbody tr td:nth-child(1) {
+            width: 20%;
+        }
+        .order-details tbody tr td:nth-child(3) {
+            width: 20%;
+        }
 
-    .invoice-header {
-        display: flex;
-        justify-content: space-between;
-        margin-bottom: 20px;
-    }
-
-    .invoice-header .invoice-id-date {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-    }
-
-    .invoice-header h1 {
-        font-size: 30px;
-        color: #000000;
-        margin: 0;
-    }
-
-    .invoice-header .invoice-id-date,
-    .invoice-header .address {
-        font-size: 14px;
-        color: #777;
-    }
-
-    .address {
-        text-align: right;
-        max-width: 50%; 
-        word-wrap: break-word; 
-    }
-
-    .details-container {
+        .text-start {
+            text-align: left;
+        }
+        .text-end {
+            text-align: right;
+        }
+        .text-center {
+            text-align: center;
+        }
+        .company-data span {
+            margin-bottom: 4px;
+            display: inline-block;
+            font-family: sans-serif;
+            font-size: 14px;
+            font-weight: 400;
+        }
+        .details-container {
         margin-top: 20px;
     }
 
@@ -68,148 +85,145 @@
         text-align: left;
     }
 
-    th {
-        background-color: #f2f2f2;
-    }
+    .white-bg {
+    background-color: #ffffff !important; /* Latar belakang putih */
+}
 
-    .invoice-items {
-        width: 100%;
-        border-collapse: collapse;
-        margin-top: 20px;
-    }
+.text-end-company-data {
+    text-align: right;/* Meratakan teks kanan */
+}
 
-    .invoice-items th, .invoice-items td {
-        border: 1px solid #ddd;
-        padding: 10px;
-        text-align: center;
-    }
 
-    .invoice-items th {
-        background-color: #f9f9f9;
-    }
+    .no-border {
+            border: 1px solid #fff !important;
+        }
+    .bg-blue {
+    background-color: #0056b3; /* Biru lebih gelap */
+    color: white;
+}
+.bg-blue th{
+    color: white
+}
+.total-row {
+    background-color: #f0f0f0;
+    font-weight: bold;
+}
 
-    .total-row {
-        font-weight: bold;
-        background-color: #f9f9f9;
-    }
-
-    .footer {
-        text-align: center;
-        margin-top: 30px;
-        color: #777;
-        font-size: 14px;
-    }
-
-    .footer p {
-        margin: 5px;
-    }
 
     </style>
 </head>
 <body>
-    <div class="invoice-container">
-        <!-- Header Section -->
-        <div class="invoice-header">
-            <div>
-                <h1>Laptop Store <br> Medan</h1>
-            </div>
-            <div class="invoice-id-date-address">
-                <div class="address">
-                    <p>Invoice ID: #{{ $order->id }}</p>
-                    <p>Date: {{ $order->updated_at->format('d M Y') }}</p>
-                    <p>Address: Komp.Garuda Mas no.4,<br>
-                    Jl.Berlian I, Deli Tua Barat</p>
-                    <p>Zip Code: 20361</p>
-                </div>
-            </div>
-        </div>
 
-        <!-- Order and User Details -->
-        <table class="details-container">
-            <tr>
-                <th colspan="2">Order Details</th>
-                <th colspan="2">User Details</th>
+    <table class="order-details">
+        <thead>
+            <tr class="white-bg">
+                <th width="50%" colspan="2">
+                    <h2 class="text-start">Laptop Store Medan</h2>
+                </th>
+                <th width="50%" colspan="2" class="text-end text-end-company-data">
+                    <span>Invoice ID: #{{ $order->id }}</span> <br>
+                    <span>Date: {{ $order->updated_at->format('d M Y') }}</span> <br>
+                    <span>Zip code : 20361</span> <br>
+                    <span>Address: Komp.Garuda Mas no.4,<br>
+                        Jl.Berlian I, Deli Tua Barat</span> <br>
+                </th>
             </tr>
+            
+            <tr class="bg-blue">
+                <th width="50%" colspan="2">Order Details</th>
+                <th width="50%" colspan="2">User Details</th>
+            </tr>
+        </thead>
+        <tbody>
             <tr>
-                <th>Order ID</th>
+                <td>Order Id</td>
                 <td>{{ $order->id }}</td>
-                <th>Customer Name</th>
+
+                <td>Customer Name</td>
                 <td>{{ $order->user->fullname }}</td>
             </tr>
             <tr>
-                <th>Payment Method</th>
+                <td>Payment Mode</td>
                 <td>{{ $order->payment->payment_method }}</td>
-                <th>Email</th>
+
+                <td>Email</td>
                 <td>{{ $order->user->email }}</td>
             </tr>
             <tr>
-                <th>Ordered at</th>
+                <td>Ordered Date</td>
                 <td>{{ $order->created_at->format('d M Y') }}</td>
-                <th>Phone</th>
+
+                <td>Phone</td>
                 <td>{{ $order->user->phone }}</td>
             </tr>
             <tr>
-                <th>Status</th>
+                <td>Status</td>
                 <td>{{ $order->status->order_status }}</td>
-                <th>Shipping Address</th>
+
+                <td>Address</td>
                 <td>{{ $order->shipping_address }}</td>
             </tr>
-        </table>
+        </tbody>
+    </table>
 
-        <div>
-            <h1>Order Items</h1>
-        <table class="invoice-items">
-            <thead>
-                <tr>
-                    <th>ID</th>
+    <table>
+        <thead>
+            <tr>
+                <th class="no-border text-start heading" colspan="5">
+                    Order Items
+                </th>
+            </tr>
+            <tr class="bg-blue">
+                <th>ID</th>
                     <th>Product</th>
                     <th>Price (Rp)</th>
                     <th>Quantity</th>
                     <th>Total (Rp)</th>
-                </tr>
-            </thead>
-            <tbody>
-                @php $totalPrice = 0; @endphp
-                @foreach ($order->orderDetails as $detail)
-                    @php 
-                        $subtotal = $detail->price * $detail->quantity;
-                        $totalPrice += $subtotal;
-                    @endphp
-                    <tr>
-                        <td>{{ $detail->product->id }}</td>
-                        <td>{{ $detail->product->product_name }}</td>
-                        <td>{{ number_format($detail->price, 0, ',', '.') }}</td>
-                        <td>{{ $detail->quantity }}</td>
-                        <td>{{ number_format($subtotal, 0, ',', '.') }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        </div>
-
-        @php
-            // Menghitung subtotal dan biaya pengiriman
-            $subtotal = $totalPrice;
-            $shippingCost = $order->total_price - $subtotal; // Menghitung biaya pengiriman
-            $total = $subtotal + $shippingCost; // Total dengan biaya pengiriman
-        @endphp
-
-        <!-- Total Section -->
-        <table class="details-container">
+            </tr>
+        </thead>
+        <tbody>
+            @php $totalPrice = 0; @endphp
+            @foreach ($order->orderDetails as $detail)
+                @php 
+                    $subtotal = $detail->price * $detail->quantity;
+                    $totalPrice += $subtotal;
+                @endphp
             <tr>
-                <th>Shipping Cost (Rp)</th>
-                <td>{{ number_format($order->total_price - $totalPrice, 0, ',', '.') }}</td>
+                <td width="10%">{{ $detail->product->id }}</td>
+                <td>{{ $detail->product->product_name }} </td>
+                <td width="10%">{{ number_format($detail->price, 0, ',', '.') }}</td>
+                <td width="10%">{{ $detail->quantity }}</td>
+                <td width="15%" class="fw-bold">{{ number_format($subtotal, 0, ',', '.') }}</td>
             </tr>
-            <tr class="total-row">
-                <th>Total (Rp)</th>
-                <td>{{ number_format($order->total_price, 0, ',', '.') }}</td>
-            </tr>
-        </table>
+            @endforeach
+        </tbody>
+    </table>
 
-        <!-- Footer -->
-        <div class="footer">
-            <p>Thank you for shopping with us :)</p>
-        </div>
-    </div>
+    @php
+    // Menghitung subtotal dan biaya pengiriman
+    $subtotal = $totalPrice;
+    $shippingCost = $order->total_price - $subtotal; // Menghitung biaya pengiriman
+    $total = $subtotal + $shippingCost; // Total dengan biaya pengiriman
+@endphp
+
+<!-- Total Section -->
+<table class="details-container">
+    <tr>
+        <th>Shipping Cost (Rp)</th>
+        <td>{{ number_format($order->total_price - $totalPrice, 0, ',', '.') }}</td>
+    </tr>
+    <tr>
+        <th>Total (Rp)</th>
+        <td class="total-heading">{{ number_format($order->total_price, 0, ',', '.') }}</td>
+    </tr>
+    
+</table>
+
+
+    <br>
+    <p class="text-center">
+        Thank your for shopping with us :)
+    </p>
+
 </body>
 </html>
