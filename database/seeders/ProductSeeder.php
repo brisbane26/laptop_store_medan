8096eb8497798d3d2eefc4f65a1cce710dd71e1e
Pangeran-28 <pangeranebenezer@gmail.com>
2024-12-03 21:31:36 +0700

    datalaptop

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -31,33 +31,330 @@ class ProductSeeder extends Seeder
         // gaamRDJEO5xNbQMfgSXx91ZNIVYxid2S110yVkKg.jpg
 
         Product::create([
-            "product_name" => "Toraja Coffee",
-            "orientation" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro debitis eius deserunt odio, repudiandae ad repellendus laboriosam nobis sed?",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem temporibus, pariatur, tempore quia officiis at repudiandae dolore assumenda sunt fugiat alias illo nam minus autem dolor voluptate. Dignissimos eum natus ipsum optio neque numquam, voluptatem autem! Officiis, voluptas. Dolorum atque minima, aliquam facilis minus exercitationem aliquid doloremque vero, error qui consequatur quas tempore aspernatur asperiores cupiditate similique? Eius esse excepturi repellat deleniti, asperiores quas magni! Labore facere dicta expedita natus quisquam eaque, aspernatur minima quas nobis mollitia soluta sed id incidunt consequatur recusandae. Asperiores distinctio cum recusandae, odit earum quod vero similique assumenda? Autem perferendis ipsa accusamus id eaque. Sapiente!",
-            "price" => 50000,
-            "stock" => 120,
-            "discount" => 5,
-            "image" => "product/gaamRDJEO5xNbQMfgSXx91ZNIVYxid2S110yVkKg.jpg",
-        ]);
-
-        Product::create([
-            "product_name" => "Arabica Coffee",
-            "orientation" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro debitis eius deserunt odio, repudiandae ad repellendus laboriosam nobis sed?",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem temporibus, pariatur, tempore quia officiis at repudiandae dolore assumenda sunt fugiat alias illo nam minus autem dolor voluptate. Dignissimos eum natus ipsum optio neque numquam, voluptatem autem! Officiis, voluptas. Dolorum atque minima, aliquam facilis minus exercitationem aliquid doloremque vero, error qui consequatur quas tempore aspernatur asperiores cupiditate similique? Eius esse excepturi repellat deleniti, asperiores quas magni! Labore facere dicta expedita natus quisquam eaque, aspernatur minima quas nobis mollitia soluta sed id incidunt consequatur recusandae. Asperiores distinctio cum recusandae, odit earum quod vero similique assumenda? Autem perferendis ipsa accusamus id eaque. Sapiente!",
-            "price" => 35000,
-            "stock" => 60,
-            "discount" => 0,
-            "image" => "product/r8e0iS6hEBocNNBRkmTy5uL7BUf9IjNSQmZrgKJy.jpg",
-        ]);
-
-        Product::create([
-            "product_name" => "Robusta Coffee",
-            "orientation" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci porro debitis eius deserunt odio, repudiandae ad repellendus laboriosam nobis sed?",
-            "description" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem temporibus, pariatur, tempore quia officiis at repudiandae dolore assumenda sunt fugiat alias illo nam minus autem dolor voluptate. Dignissimos eum natus ipsum optio neque numquam, voluptatem autem! Officiis, voluptas. Dolorum atque minima, aliquam facilis minus exercitationem aliquid doloremque vero, error qui consequatur quas tempore aspernatur asperiores cupiditate similique? Eius esse excepturi repellat deleniti, asperiores quas magni! Labore facere dicta expedita natus quisquam eaque, aspernatur minima quas nobis mollitia soluta sed id incidunt consequatur recusandae. Asperiores distinctio cum recusandae, odit earum quod vero similique assumenda? Autem perferendis ipsa accusamus id eaque. Sapiente!",
-            "price" => 55000,
-            "stock" => 70,
+            "product_name" => "ASUS ROG Zephyrus G14",
+            "orientation" => "Laptop gaming premium dengan performa tinggi, ideal untuk gamer dan kreator konten.",
+            "description" => "
+                Spesifikasi ASUS ROG Zephyrus G14:
+                - Prosesor: AMD Ryzen 9 7940HS
+                - Kartu Grafis: NVIDIA GeForce RTX 4060, 8GB GDDR6  
+                - RAM: 16GB DDR5 (upgradeable hingga 32GB)  
+                - Penyimpanan: 1TB PCIe 4.0 NVMe SSD  
+                - Layar: 14-inch QHD, 165Hz, 100% DCI-P3, Pantone Validated  
+                - Sistem Operasi: Windows 11 Home  
+                - Baterai: 4-cell 76WHr (hingga 10 jam penggunaan ringan)  
+                - Port: USB-C (2x), USB-A (2x), HDMI 2.1, 3.5mm Audio Jack  
+                - Konektivitas: WiFi 6E, Bluetooth 5.2  
+                - Fitur Tambahan: AniMe Matrix LED Display di cover, sistem pendingin ROG Intelligent Cooling, dan Dolby Atmos Audio.  
+            ",
+            "price" => 25000000,
+            "stock" => 50,
             "discount" => 10,
-            "image" => "product/Gy6UVqa000obrsMGJaRAzZ4hWEz5WGhu38QawLzC.jpg",
+            "image" => "product/ASUS_ROG_Zephyrus_G14.jpg",
         ]);
+        
+
+        Product::create([
+            "product_name" => "Dell XPS 13 Plus",
+            "orientation" => "Ultrabook premium untuk produktivitas tinggi.",
+            "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-1360P  
+            - RAM: 16GB LPDDR5  
+            - Penyimpanan: 512GB NVMe SSD  
+            - Layar: 13.4-inch OLED 3.5K Touchscreen  
+            - Baterai: 10 jam penggunaan ringan   
+            ",
+            "price" => 24000000,
+            "stock" => 40,
+            "discount" => 5,
+            "image" => "product/Dell_XPS_13_Plus.jpg",
+        ]);
+        
+        Product::create([
+            "product_name" => "MacBook Pro 14-inch M2 Pro",
+            "orientation" => "Laptop premium dari Apple dengan chip M2 Pro.",
+            "description" => "
+            Spesifikasi:
+            - Chipset: Apple M2 Pro (10-core CPU, 16-core GPU)  
+            - RAM: 16GB Unified Memory  
+            - Penyimpanan**: 512GB SSD  
+            - Layar**: 14.2-inch Liquid Retina XDR  
+            - Baterai**: Hingga 17 jam  
+        ",
+        "price" => 28000000,
+        "stock" => 30,
+        "discount" => 7,
+        "image" => "product/MacBook_Pro_14-inch_M2_Pro.jpg",
+        ]);
+        Product::create([
+            "product_name" => "HP Spectre x360 14",
+        "orientation" => "Laptop 2-in-1 dengan desain elegan.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-1260P  
+            - RAM: 16GB LPDDR4x  
+            - Penyimpanan: 1TB NVMe SSD  
+            - Layar: 13.5-inch OLED 3K Touchscreen  
+            - Fitur: Pena Stylus, WiFi 6E  
+        ",
+        "price" => 23000000,
+        "stock" => 25,
+        "discount" => 8,
+        "image" => "product/HP_Spectre_x360.jpeg",
+        ]);
+
+        Product::create([
+        "product_name" => "Lenovo Legion 5 Pro",
+        "orientation" => "Laptop gaming dengan performa tinggi.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: AMD Ryzen 7 6800H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3070 Ti  
+            - RAM: 16GB DDR5  
+            - Penyimpanan: 512GB NVMe SSD  
+            - Layar: 16-inch WQXGA, 165Hz  
+        ",
+        "price" => 22000000,
+        "stock" => 60,
+        "discount" => 10,
+        "image" => "product/Lenovo_Legion_5_Pro.jpeg",
+        ]);
+
+        Product::create([
+        "product_name" => "Acer Swift 3 OLED",
+        "orientation" => "Laptop ringan dengan layar OLED.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i5-1240P  
+            - RAM: 16GB LPDDR5  
+            - Penyimpanan**: 512GB SSD  
+            - Layar: 14-inch 2.8K OLED  
+            - Baterai: Hingga 12 jam  
+        ",
+        "price" => 15000000,
+        "stock" => 70,
+        "discount" => 5,
+        "image" => "product/Acer_Swift_3_OLED.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "ASUS TUF Gaming F15",
+        "orientation" => "Laptop gaming tangguh dengan harga terjangkau.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-12700H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3050  
+            - RAM: 16GB DDR4  
+            - Penyimpanan: 512GB SSD  
+            - Layar: 15.6-inch FHD, 144Hz  
+        ",
+        "price" => 17000000,
+        "stock" => 80,
+        "discount" => 15,
+        "image" => "product/ASUS_TUF_F15.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "Razer Blade 15 Advanced",
+        "orientation" => "Laptop gaming premium dengan desain ramping.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-12800H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3070 Ti  
+            - RAM: 16GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 15.6-inch QHD, 240Hz  
+        ",
+        "price" => 35000000,
+        "stock" => 20,
+        "discount" => 12,
+        "image" => "product/Razer_Blade_15.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "Microsoft Surface Laptop 5",
+        "orientation" => "Laptop ultrabook untuk produktivitas tinggi.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-1265U  
+            - RAM: 16GB LPDDR5  
+            - Penyimpanan: 512GB SSD  
+            - Layar: 13.5-inch PixelSense Display, 3:2  
+            - Baterai: Hingga 18 jam  
+        ",
+        "price" => 22000000,
+        "stock" => 40,
+        "discount" => 7,
+        "image" => "product/Microsoft_Surface_Laptop_5.jpeg",
+        ]);
+
+        Product::create([
+        "product_name" => "Acer Predator Helios 300",
+        "orientation" => "Laptop gaming untuk performa berat.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-13700H  
+            - Kartu Grafis: NVIDIA GeForce RTX 4060  
+            - RAM: 16GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 15.6-inch QHD, 165Hz  
+        ",
+        "price" => 25000000,
+        "stock" => 30,
+        "discount" => 10,
+        "image" => "product/Acer_Predator_Helios_300.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "ASUS VivoBook Pro 16X",
+        "orientation" => "Laptop untuk kreator konten.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: AMD Ryzen 9 6900HX  
+            - Kartu Grafis: NVIDIA GeForce RTX 3050 Ti  
+            - RAM**: 16GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 16-inch 4K OLED HDR  
+        ",
+        "price" => 23000000,
+        "stock" => 45,
+        "discount" => 8,
+        "image" => "product/ASUS_VivoBook_Pro_16X.png",
+         ]);
+
+        Product::create([
+        "product_name" => "Lenovo ThinkPad X1 Carbon Gen 11",
+        "orientation" => "Ultrabook premium untuk profesional.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-1365U  
+            - RAM: 16GB LPDDR5  
+            - Penyimpanan: 512GB SSD  
+            - Layar: 14-inch WUXGA Touchscreen  
+            - Baterai: Hingga 15 jam  
+        ",
+        "price" => 26000000,
+        "stock" => 35,
+        "discount" => 10,
+        "image" => "product/Lenovo_ThinkPad_X1_Carbon.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "MSI Katana GF66",
+        "orientation" => "Laptop gaming dengan harga terjangkau.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i5-12500H  
+            - Kartu Grafis**: NVIDIA GeForce RTX 3050 Ti  
+            - RAM: 8GB DDR4  
+            - Penyimpanan: 512GB SSD  
+            - Layar: 15.6-inch FHD, 144Hz  
+        ",
+        "price" => 15000000,
+        "stock" => 50,
+        "discount" => 12,
+        "image" => "product/MSI_Katana_GF66.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "Dell Alienware x14",
+        "orientation" => "Laptop gaming tipis dan ringan.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-12700H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3060  
+            - RAM: 16GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 14-inch FHD, 144Hz  
+        ",
+        "price" => 28000000,
+        "stock" => 25,
+        "discount" => 10,
+        "image" => "product/Dell_Alienware_x14.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "HP Omen 16",
+        "orientation" => "Laptop gaming dengan layar besar.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: AMD Ryzen 7 6800H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3070  
+            - RAM: 16GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 16.1-inch QHD, 165Hz  
+        ",
+        "price" => 24000000,
+        "stock" => 40,
+        "discount" => 9,
+        "image" => "product/HP_Omen_16.png",
+        ]);
+
+        Product::create([
+        "product_name" => "Gigabyte Aero 16",
+        "orientation" => "Laptop kreator dengan layar OLED.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i9-12900H  
+            - Kartu Grafis: NVIDIA GeForce RTX 3070 Ti  
+            - RAM: 32GB DDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 16-inch 4K AMOLED  
+        ",
+        "price" => 35000000,
+        "stock" => 15,
+        "discount" => 8,
+        "image" => "product/Gigabyte_Aero_16.jpeg",
+        ]);
+
+        Product::create([
+        "product_name" => "ASUS ZenBook Duo 14",
+        "orientation" => "Laptop dengan dua layar untuk multitasking.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-12500H  
+            - RAM**: 16GB LPDDR5  
+            - Penyimpanan: 1TB SSD  
+            - Layar: 14-inch FHD + ScreenPad Plus  
+        ",
+        "price" => 24000000,
+        "stock" => 30,
+        "discount" => 10,
+        "image" => "product/ASUS_ZenBook_Duo_14.png",
+        ]);
+
+        Product::create([
+        "product_name" => "Razer Book 13",
+        "orientation" => "Ultrabook untuk produktivitas dengan desain ramping.",
+        "description" => "
+            Spesifikasi:
+            - Prosesor: Intel Core i7-1165G7  
+            - RAM: 16GB LPDDR4x  
+            - Penyimpanan: 512GB SSD  
+            - Layar: 13.4-inch UHD Touchscreen  
+        ",
+        "price" => 21000000,
+        "stock" => 45,
+        "discount" => 5,
+        "image" => "product/Razer_Book_13.jpg",
+        ]);
+
+        Product::create([
+        "product_name" => "Apple MacBook Air M2",
+        "orientation" => "Laptop ultrabook dengan performa efisien.",
+        "description" => "
+            Spesifikasi:
+            - Chipset: Apple M2 (8-core CPU, 8-core GPU)  
+            - RAM: 8GB Unified Memory  
+            - Penyimpanan**: 512GB SSD  
+            - Layar: 13.6-inch Liquid Retina  
+        ",
+        "price" => 19000000,
+        "stock" => 50,
+        "discount" => 6,
+        "image" => "product/MacBook_Air_M2.jpeg",
+        ]);
+
+
     }
 }
