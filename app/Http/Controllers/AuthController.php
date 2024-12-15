3951a17f43fd97442fdc3907eaf364995fe75409
petrr4 <petraik111@gmail.com>
2024-12-15 23:20:20 +0700

    regis

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\{Auth, Hash};
+use Illuminate\Support\Str;
+
 
 class AuthController extends Controller
 {
